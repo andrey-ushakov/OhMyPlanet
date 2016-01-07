160f5d1a7d236912ca0e45b746a84b63cb3288c2
AndreyU <andrey200964@yandex.ru>
2016-01-07 21:14:21 +0100

    add UE4Duino plugin

---
+++ Unreal/Plugins/UE4Duino/Intermediate/Build/Win64/UE4Editor/Development/UE4Duino/PCH.UE4DuinoPCH.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\raghu\Documents\ArduinoTest\ArduinoTest 4.9\Plugins\UE4Duino\Source\UE4Duino\Private\UE4DuinoPCH.h"
---
+++ Unreal/Plugins/UE4Duino/Intermediate/Build/Win64/UE4Editor/Development/UE4Duino/UE4DuinoPCH.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_UE4DUINOPCH_H__
+#define __AUTO_UE4DUINOPCH_H__
+//Last Write: 07-09-2015 1.02.35 PM +00:00
+#include "C:\Users\raghu\Documents\ArduinoTest\ArduinoTest 4.9\Plugins\UE4Duino\Source\UE4Duino\Private\UE4DuinoPCH.h"
+#endif//__AUTO_UE4DUINOPCH_H__
